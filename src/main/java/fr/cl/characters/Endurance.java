936d7c705947fbbf7b73f49c76b0bdaa198784e1
Maxime Rodriguez <max.rodriguezidf@gmail.com>
2023-11-05 12:25:50 +0100

    Ajout du seuil de douleur

--- src/main/java/fr/cl/characters/Endurance.java
+++ src/main/java/fr/cl/characters/Endurance.java
@@ -5,9 +5,20 @@ package fr.cl.characters;
  */
 public class Endurance {
 
+    /**
+     * Maximum de point de vie
+     */
     private int maximum;
 
+    /**
+     * Points de vie courant
+     */
     private int current;
+
+    /**
+     * Seuil de douleur
+     */
+    private int douleur;
 
     public Endurance(int maximum) {
         if (maximum < 1) {
@@ -51,6 +62,17 @@ public class Endurance {
             throw new IllegalArgumentException("La vie ne peut pas être à 0 ou moins");
         }
         this.maximum = maximum;
+    }
+
+    /**
+     * Renvoi si les dégats sont plus important que le seuil de douleur ou non
+     *
+     * @param damage dégats infligés
+     * @return <code>true</code> si les dommages sont plus important que le seuil de douleur,
+     * <code>false</code> si égal ou inférieur
+     */
+    public boolean douleur(int damage) {
+        return douleur > damage;
     }
 
 }
