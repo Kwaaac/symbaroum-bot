98a22711cf16eebc7f0998452ebf105df17f5191
Maxime Rodriguez <max.rodriguezidf@gmail.com>
2023-11-05 12:19:17 +0100

    Add class endurance qui représente les points de vie d'un personnage

---
+++ src/main/java/fr/cl/characters/Endurance.java
@@ -0,0 +1,56 @@
+package fr.cl.characters;
+
+/**
+ * Classe qui représente l'endurance d'un personnage, plus vulgairement ses points de vie.
+ */
+public class Endurance {
+
+    private int maximum;
+
+    private int current;
+
+    public Endurance(int maximum) {
+        if (maximum < 1) {
+            throw new IllegalArgumentException("La vie ne peut pas être à 0 ou moins");
+        }
+
+        this.maximum = maximum;
+        this.current = maximum;
+    }
+
+    /**
+     * Méthode qui applique des dommages à la vie.
+     * Renvoie <code>true</code> si les points de vie atteignent 0 <code>false</code> sinon
+     *
+     * @param damage nombre de dommage à infliger aux points de vies
+     * @return <code>true</code> si les points de vie atteignent 0 <code>false</code> sinon
+     */
+    public boolean damage(int damage) {
+        current = Math.max(0, current - damage);
+
+        return current == 0;
+    }
+
+    /**
+     * Soigne les points de vie à hauteur de heal
+     *
+     * @param heal nombre de points de vie à restaurer
+     */
+    public void heal(int heal) {
+        var healed = current + heal;
+        current = Math.min(healed, maximum);
+    }
+
+    /**
+     * Met à jour les points de vie maximum
+     *
+     * @param maximum points de vie
+     */
+    public void updateEndurance(int maximum) {
+        if (maximum < 1) {
+            throw new IllegalArgumentException("La vie ne peut pas être à 0 ou moins");
+        }
+        this.maximum = maximum;
+    }
+
+}
