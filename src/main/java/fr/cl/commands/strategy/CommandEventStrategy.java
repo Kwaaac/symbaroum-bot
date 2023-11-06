c731096813ae0d7e8d4d45b583c50606d90f24c5
Maxime Rodriguez <max.rodriguezidf@gmail.com>
2023-11-06 17:18:17 +0100

    Commentaires

--- src/main/java/fr/cl/commands/strategy/CommandEventStrategy.java
+++ src/main/java/fr/cl/commands/strategy/CommandEventStrategy.java
@@ -3,11 +3,21 @@ package fr.cl.commands.strategy;
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
 import reactor.core.publisher.Mono;
 
+import java.util.NoSuchElementException;
+
 @FunctionalInterface
 public interface CommandEventStrategy {
     CommandEventStrategy defaultReponse = ev -> ev
             .reply("Oulah, y'a eu une bille, va crier fort dans un channel, peut-être que quelqu'un t'entendra")
             .withEphemeral(true);
 
-    Mono<Void> apply(ChatInputInteractionEvent event);
+    /**
+     * Applique la lambda sur un evenement et renvoi un Consumer de void (Mono => 1 élement ou rien, ici c'est rien)
+     * Généralement pour répondre à la commande
+     *
+     * @param event
+     * @return Mono<Void> qui fait une action sur l'event
+     * @throws NoSuchElementException Si une tentative de récuperer une option se solde par un échec
+     */
+    Mono<Void> apply(ChatInputInteractionEvent event) throws NoSuchElementException;
 }
