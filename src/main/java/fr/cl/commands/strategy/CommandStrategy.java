033e9716c56e10e76f0e8f04e45cda8c229aa933
Maxime Rodriguez <max.rodriguezidf@gmail.com>
2023-11-06 08:09:14 +0100

    Ajout de l'intercage CommandStrategy

---
+++ src/main/java/fr/cl/commands/strategy/CommandStrategy.java
@@ -0,0 +1,13 @@
+package fr.cl.commands.strategy;
+
+import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
+import reactor.core.publisher.Mono;
+
+@FunctionalInterface
+public interface CommandStrategy {
+    CommandStrategy defaultReponse = ev -> ev
+            .reply("Oulah, y'a eu une bille, va crier fort dans un channel, peut-être que quelqu'un t'entendra")
+            .withEphemeral(true);
+
+    Mono<Void> apply(ChatInputInteractionEvent event);
+}
